ff53ea378135e551b85a76b55db7a362f9a21191
Satinder Sandhu <satinder.sandhu@securityassured.ca>
2026-09-22 15:51:28 -0400

    Add site navigation

---
+++ app/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  index: { title: 'Home', type: 'page', display: 'hidden' },
+  research: { title: 'Research' },
+  experiments: { title: 'Experiments' },
+  methodology: { title: 'Methodology' },
+  about: { title: 'About' }
+}
