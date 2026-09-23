d898aba5bf84148095a1e58e787dc2d76011d4e6
Satinder Sandhu <satinder.sandhu@securityassured.ca>
2026-09-22 20:30:34 -0400

    Use exact uploaded Guardian Lab banner asset

--- app/GuardianBanner.jsx
+++ app/GuardianBanner.jsx
@@ -1,17 +1,9 @@
-import chunk1 from './banner-data/chunk1'
-import chunk2 from './banner-data/chunk2'
-import chunk3 from './banner-data/chunk3'
-import chunk4 from './banner-data/chunk4'
-import chunk5 from './banner-data/chunk5'
-
-const bannerSrc = `data:image/webp;base64,${chunk1}${chunk2}${chunk3}${chunk4}${chunk5}`
-
 export default function GuardianBanner() {
   return (
     <section className="brand-banner-section" aria-label="Guardian Lab identity">
       <img
         className="brand-banner"
-        src={bannerSrc}
+        src="/guardian-lab-banner-exact.png"
         width="2048"
         height="682"
         loading="eager"
